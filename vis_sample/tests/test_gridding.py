a12eff94ded2ef6bbb9125d9351ed3cb8f036dec
j6626 <janehuang212@gmail.com>
2018-12-23 00:19:51 -0500

    fixed tests

--- vis_sample/tests/test_gridding.py
+++ vis_sample/tests/test_gridding.py
@@ -5,5 +5,5 @@ def test_spheroid():
     assert gridding.spheroid(eta=100) ==1.e30
     assert gridding.spheroid(eta=-100) ==1.e30
     assert gridding.spheroid(eta = 1) == 0.01466325
-    assert gridding.spheroid(eta = -1) == -0.01466325
+    assert gridding.spheroid(eta = -1) == 0.01466325
     assert gridding.spheroid(eta = 2) == (0.01624782*3**6 + -0.05350728*3**5 + 0.1464354*3**4 + -0.2347118*3**3 + 0.2180684*3**2 + -0.09858686*3 + 0.01466325)/(0.2177793*3 + 1)
