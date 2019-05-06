8152e641e7b422e7bd34f7043eb2573ef79f133f
j6626 <janehuang212@gmail.com>
2019-05-05 23:14:01 -0400

    edited setup.py file to retain compatibiltiy with Python 2.7

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup (
     install_requires = [
         'python >= 2.7.0',
         'numpy >= 1.13.1',
-        'scipy >= 0.19.1',
+        'scipy >= 0.19.1,<=1.2.0',
         'astropy >= 1.0.0,<3.0.0'
     ],
 
